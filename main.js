c1a7f2b9ccf6203cef6fa08b9a62eafc1bf7552a
juba1508 <jule.bauer96@web.de>
2023-03-30 17:14:09 +0200

    code Kosmetik

--- main.js
+++ main.js
@@ -107,12 +107,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
     for(let stop of STOPS){
-        console.log(stop);
-        console.log(stop.title);
-        console.log(stop.user);
-        console.log(stop.lat);
-        console.log(stop.lng);
-        console.log(stop.wikipedia);
+        //Marker für den Stop
 
         L.marker([stop.lat, stop.lng]).addTo(map)
         .bindPopup(stop.title)
